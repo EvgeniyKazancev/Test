48768f37d0edb737558904dd4f1fb8a07903439a
EvgeniyKazancev <ekazancev777@gmail.com>
2022-07-25 22:46:21 +0300

    up

---
+++ Loader/src/Test/MusicalInstrument.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class MusicalInstrument {
+    String name, manufacturer;
+    int price;
+
+    public MusicalInstrument(String name, String manufacturer, int price) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.price = price;
+    }
+    public String info(){
+        return name + " " + manufacturer + " " + price;
+
+    }
+}
---
+++ Loader/src/Test/Stringed.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Stringed extends MusicalInstrument{
+    int quantityOfStrings;
+
+    public Stringed(String name, String manufacturer, int price, int quantityOfStrings) {
+        super(name, manufacturer, price);
+        this.quantityOfStrings = quantityOfStrings;
+    }
+    public String play(){
+        return name + " sounds beautiful";
+    }
+}
