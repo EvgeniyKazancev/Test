875b2f179813b350159c20a91dc1c109b73a23a3
EvgeniyKazancev <ekazancev777@gmail.com>
2022-11-20 22:39:57 +0300

    up

---
+++ Loader/src/LeetCode/Solution1.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class Solution1 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0)
+            return "";
+        String prefix = strs[0];
+        for (var str : strs)
+            while (str.indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty())
+                    return "";
+            }
+            return prefix;
+    }
+}
