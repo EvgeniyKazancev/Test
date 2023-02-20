1e89261549e9b63b4de919f7ffc4a672f57ec974
EvgeniyKazancev <ekazancev777@gmail.com>
2023-02-20 15:20:06 +0300

    up

--- Loader/src/Calculator.java
+++ Loader/src/Calculator.java
@@ -47,6 +47,9 @@ public class Calculator {
         if (matcher1.find() && matcher2.find()) {
             int firstNum = Integer.parseInt(cheks[0]);
             int secondNum = Integer.parseInt(cheks[2]);
+            if (firstNum < 1 || firstNum > 10 || secondNum < 1 || secondNum > 10) {
+                throw new IllegalArgumentException();
+            }
             res = calculate(cheks, firstNum, secondNum);
             return "" + res;
         } else if (romanNumeralMap.containsKey(cheks[0].toUpperCase()) &&
@@ -63,6 +66,8 @@ public class Calculator {
                         return entry.getKey();
                     }
                 }
+            } else if (res > 10) {
+                return toRoman(res);
             }
             throw new Exception();
         } else
@@ -94,4 +99,32 @@ public class Calculator {
         return res;
     }
 
+    private static int[] intervals = {0, 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    private static String[] numerals = {"", "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+
+    private final static int findFloor(final int number, final int firstIndex, final int lastIndex) {
+        if (firstIndex == lastIndex)
+            return firstIndex;
+        if (intervals[firstIndex] == number)
+            return firstIndex;
+        if (intervals[lastIndex] == number)
+            return lastIndex;
+        final int median = (lastIndex + firstIndex) / 2;
+        if (median == firstIndex)
+            return firstIndex;
+        if (number == intervals[median])
+            return median;
+        if (number > intervals[median])
+            return findFloor(number, median, lastIndex);
+        else
+            return findFloor(number, firstIndex, median);
+
+    }
+
+    public final static String toRoman(final int number) {
+        int floorIndex = findFloor(number, 0, intervals.length - 1);
+        if (number == intervals[floorIndex])
+            return numerals[floorIndex];
+        return numerals[floorIndex] + toRoman(number - intervals[floorIndex]);
+    }
 }
